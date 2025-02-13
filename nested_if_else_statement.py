5dc7ab5c0b2c60621e487ca78051177495163177
jonaspalima <palimajonas_bsit@plmun.edu.ph>
2025-02-13 20:27:51 +0800

    Update nested_if_else_statement.py

--- nested_if_else_statement.py
+++ nested_if_else_statement.py
@@ -5,10 +5,13 @@ try:
     membership = membership_str.strip().lower()
 
     if age >= 18:
+        
     if membership == "yes":
     print("Access granted.")
+    
     else:
     print("Membership required for access.")
+
     else:
     print("Access denied. Must be at least 18 years old.")
 
